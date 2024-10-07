c1362edcd812a0a02448dbe9ba5ee044730c7738
75008Kajal <128771800+75008Kajal@users.noreply.github.com>
2024-10-07 11:39:48 +0530

    test

---
+++ frontend/kyc/.vite/deps_temp_488f8a7e/react_jsx-runtime.js
@@ -0,0 +1,7 @@
+import {
+  require_jsx_runtime
+} from "./chunk-XC26ZK3T.js";
+import "./chunk-E55NSNTN.js";
+import "./chunk-4MBMRILA.js";
+export default require_jsx_runtime();
+//# sourceMappingURL=react_jsx-runtime.js.map
