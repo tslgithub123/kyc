152ff752ef0391a9b0dff2d2dbb3ff052cb7ae07
Dhananjay Yelwande <163529207+tslgithub123@users.noreply.github.com>
2024-10-11 17:42:53 +0530

    Remove GeneralInformationForm component and add UserInformation component. Add Custom Modal component for User Information display. Multi use.

--- frontend/kyc/src/utils/colorUtils.ts
+++ frontend/kyc/src/utils/colorUtils.ts
@@ -1,14 +1,20 @@
-export const getUserTypeColor = (userType: string, strength?: string, isMantine?: boolean): string => {
+export const getUserTypeColor = (userType: string, strength?: string): string => {
     let myColor = '';
-    if (userType === 'env') {
+    if (userType === 'TSL') {
+        myColor = 'red';
+    } else if (userType === 'Director') {
+        myColor = 'violet';
+    } else if (userType === 'Administrator') {
+        myColor = 'orange';
+    } else if (userType === 'Environment Officer') {
         myColor = 'green';
-    } else if (userType === 'man') {
+    } else if (userType === 'Manager') {
         myColor = 'yellow';
-    } else if (userType === 'thp') {
+    } else if (userType === 'Third Party') {
         myColor = 'grape';
     } else {
         myColor = 'gray';
     }
-    const baseColor = isMantine ? `var(--mantine-color-${myColor}` : `${myColor}`;
+    const baseColor = strength ? `var(--mantine-color-${myColor}` : `${myColor}`;
     return strength ? `${baseColor}-${strength}` : `${baseColor}`;
 };
