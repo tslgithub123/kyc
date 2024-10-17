1185dd841cbec9d487937edada5fbafb08a61b8c
Omkar0109 <154223483+Omkar0109@users.noreply.github.com>
2024-10-17 17:23:24 +0530

    Remove Import statement

--- backend/kyc/src/main/java/com/tsl/kyc/service/CompanyProfileService.java
+++ backend/kyc/src/main/java/com/tsl/kyc/service/CompanyProfileService.java
@@ -10,7 +10,6 @@ import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
