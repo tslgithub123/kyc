12ca779fe07a4d51731c1b3879304a5406c19414
Dhananjay Yelwande <163529207+tslgithub123@users.noreply.github.com>
2024-10-24 18:26:23 +0530

    Add StartupConfig and NotificationTypes classes. Add Notification Builder Factory

--- backend/kyc/src/main/java/com/tsl/kyc/config/NotificationFactoryConfig.java
+++ backend/kyc/src/main/java/com/tsl/kyc/config/NotificationFactoryConfig.java
@@ -2,19 +2,27 @@ package com.tsl.kyc.config;
 
 import com.tsl.kyc.factory.ClientNotificationFactory;
 import com.tsl.kyc.factory.NotificationFactory;
+import com.tsl.kyc.utils.NotificationTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
 public class NotificationFactoryConfig {
 
-    @Bean
-    public Map<String, NotificationFactory> factoryMap(ClientNotificationFactory clientRegistrationNotificationFactory) {
+    @Bean(name = "notificationFactoryMap")
+    @Primary
+    public Map<String, NotificationFactory> factoryMap(ClientNotificationFactory clientNotificationFactory) {
         Map<String, NotificationFactory> factoryMap = new HashMap<>();
-        factoryMap.put("Client Registration Request", clientRegistrationNotificationFactory);
+        factoryMap.put(NotificationTypes.CLIENT_REGISTRATION_REQUEST, clientNotificationFactory);
+        factoryMap.put(NotificationTypes.DOCUMENT_UPLOAD, clientNotificationFactory);
+        factoryMap.put(NotificationTypes.APPROVAL_REQUEST, clientNotificationFactory);
         return factoryMap;
     }
 }
--- backend/kyc/src/main/java/com/tsl/kyc/factory/NotificationFactoryProvider.java
+++ backend/kyc/src/main/java/com/tsl/kyc/factory/NotificationFactoryProvider.java
@@ -1,9 +1,12 @@
 package com.tsl.kyc.factory;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 @Component
 public class NotificationFactoryProvider {
@@ -11,11 +14,20 @@ public class NotificationFactoryProvider {
     private final Map<String, NotificationFactory> factoryMap;
 
     @Autowired
-    public NotificationFactoryProvider(Map<String, NotificationFactory> factoryMap) {
+    public NotificationFactoryProvider(@Qualifier("notificationFactoryMap") Map<String, NotificationFactory> factoryMap) {
         this.factoryMap = factoryMap;
     }
 
     public NotificationFactory getFactory(String type) {
-        return factoryMap.get(type);
+        NotificationFactory factory = factoryMap.get(type);
+        if (factory == null) {
+            throw new IllegalArgumentException("No factory found for type: '" + type
+                    + "'. Available types: " + factoryMap.keySet());
+        }
+        return factory;
+    }
+
+    public Set<String> getAvailableTypes() {
+        return factoryMap.keySet();
     }
 }
